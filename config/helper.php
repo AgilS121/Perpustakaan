2cb3d886a82e7c30b00fe4d65d10524c5036af82
AgilS121 <gilsatria121@gmail.com>
2023-04-08 15:03:28 +0700

    Perpustakaan by Agils

--- config/helper.php
+++ config/helper.php
@@ -1,4 +1,4 @@
 <?php
 // pembuatan konstanta atau pendefisian agar lebih ringkas ketika dipanggil
-define('BASEURL', 'http://localhost:8080/JWD/perpustakaan/');  
+define('BASEURL', 'http://localhost:99/JWD/perpustakaan/');  
 define('APPNAME', 'WP - WEB PERPUSTAKAAN');
